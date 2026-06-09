6c2e377e083140af96bda46dfe736e8456d25735
mazenwaled40 <mazenwaled780@gmail.com>
2026-06-10 02:33:23 +0300

    Update database.py

--- school_guard/database.py
+++ school_guard/database.py
@@ -8,13 +8,17 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"sslmode": "require"},
+    pool_pre_ping=True,
+    pool_recycle=300
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-# Dependency
 def get_db():
     db = SessionLocal()
     try:
